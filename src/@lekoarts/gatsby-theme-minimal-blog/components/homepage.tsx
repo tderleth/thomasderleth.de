880fdcdc4c91cbf2d5d1b487b06d901ec98d46c2
tderleth <thomas.derleth@me.com>
2020-03-05 13:18:36 +0100

    feat: overwrote theme with shadow option

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.tsx
@@ -0,0 +1,49 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+import { Link } from "gatsby"
+import Layout from "../../../../node_modules/@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
+// @ts-ignore
+import Hero from "../texts/hero"
+// @ts-ignore
+import Bottom from "../texts/bottom"
+import Title from "../../../../node_modules/@lekoarts/gatsby-theme-minimal-blog/src/components/title"
+import Listing from "../../../../node_modules/@lekoarts/gatsby-theme-minimal-blog/src/components/listing"
+import List from "../../../../node_modules/@lekoarts/gatsby-theme-minimal-blog/src/components/list"
+import useMinimalBlogConfig from "../../../../node_modules/@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-minimal-blog-config"
+import replaceSlashes from "../../../../node_modules/@lekoarts/gatsby-theme-minimal-blog/src/utils/replaceSlashes"
+
+type PostsProps = {
+  posts: {
+    slug: string
+    title: string
+    date: string
+    excerpt: string
+    description: string
+    timeToRead: number
+    tags?: {
+      name: string
+      slug: string
+    }[]
+  }[]
+}
+
+const Homepage = ({ posts }: PostsProps) => {
+  const { basePath, blogPath } = useMinimalBlogConfig()
+
+  return (
+    <Layout>
+      <section sx={{ mb: [5, 6, 7], p: { fontSize: [1, 2, 3], mt: 2 } }}>
+        <Hero />
+      </section>
+      <Title text="Latest Posts">
+        <Link to={replaceSlashes(`/${basePath}/${blogPath}`)}>Read all posts</Link>
+      </Title>
+      <Listing posts={posts} showTags={false} />
+      <List>
+        <Bottom />
+      </List>
+    </Layout>
+  )
+}
+
+export default Homepage
