cbbeaee921bd940f9717ee6c8c33e2e03f1ca28e
Thomas Derleth <thomas.derleth@me.com>
2021-06-10 15:31:32 +0200

    updated config and deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,40 +19,20 @@ module.exports = {
       resolve: '@lekoarts/gatsby-theme-minimal-blog',
       options: {
         navigation: [
-          {
-            title: 'Blog',
-            slug: '/blog',
-          },
-          {
-            title: 'About',
-            slug: '/about',
-          },
+          { title: 'Blog', slug: '/blog' },
+          { title: 'About', slug: '/about' },
         ],
         externalLinks: [
-          {
-            name: 'CV',
-            url: '/files/curriculum-vitae.pdf',
-          },
-          {
-            name: 'GitHub',
-            url: 'https://github.com/tderleth',
-          },
-          {
-            name: 'LinkedIn',
-            url: 'https://www.linkedin.com/in/thomasderleth',
-          },
-          {
-            name: 'Twitter',
-            url: 'https://twitter.com/GtzBer',
-          },
+          { name: 'CV', url: '/files/curriculum-vitae.pdf' },
+          { name: 'GitHub', url: 'https://github.com/tderleth' },
+          { name: 'LinkedIn', url: 'https://www.linkedin.com/in/thomasderleth' },
+          { name: 'Twitter', url: 'https://twitter.com/GtzBer' },
         ],
       },
     },
     {
       resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: 'UA-66897931-1',
-      },
+      options: { trackingId: 'UA-66897931-1' },
     },
     'gatsby-plugin-sitemap',
     {
@@ -63,16 +43,8 @@ module.exports = {
           "Hello, I'm Thomas Derleth. I like to read, hear, talk, think and write about code as well as life and how to improve it",
         display: 'standalone',
         icons: [
-          {
-            src: '/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
+          { src: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
         ],
         name: 'thomasderleth.de',
         short_name: 'thomasderleth.de',
